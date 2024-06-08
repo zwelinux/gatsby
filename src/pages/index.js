e991008402664a8373f0badd8a3e620e049b84d7
zwelinux <zwelinux@gmail.com>
2024-06-08 21:25:16 +0630

    update 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,23 @@
 // Step 1: Import React
 import * as React from 'react'
-import { StaticImage } from 'gatsby-plugin-image'
+import { useStaticQuery, graphql } from 'gatsby'
 
 // Step 2: Define your component
 const IndexPage = () => {
+
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
   return (
     <main>
-      <h1>Welcome to my Gatsby site!</h1>
-      <p>I'm making this by following the Gatsby Tutorial.</p>
-      <StaticImage
-        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
-        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
-      />
+      <header>{data.site.siteMetadata.title}</header>
     </main>
   )
 }
